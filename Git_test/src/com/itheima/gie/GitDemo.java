a01f3f89fa485eb37ca5ce61f8b2b975edabed4c
ssd <ssd147305@qq.com>
2019-06-11 20:45:47 +0800

    clone修改

--- Git_test/src/com/itheima/gie/GitDemo.java
+++ Git_test/src/com/itheima/gie/GitDemo.java
@@ -4,6 +4,9 @@ public class GitDemo {
     public static void main(String[] args){
         int a=10;
        System.out.println(123456);
+        System.out.println(1234566);
+        System.out.println(123456);
+
     }
 
 }
